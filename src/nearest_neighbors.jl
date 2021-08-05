15763007610976202ee7ef40f57aef6288ad2acf
Benoît Legat <benoit.legat@gmail.com>
2021-08-04 22:12:14 -0400

    Fixes to NN

--- src/nearest_neighbors.jl
+++ src/nearest_neighbors.jl
@@ -1,5 +1,7 @@
 module NN
 
+using StaticArrays, NearestNeighbors
+
 struct GridTree{N,T}
     a::SVector{N,T}
     b::SVector{N,T}
@@ -8,6 +10,31 @@ end
 function index(g::GridTree, p::SVector)
     return Int.(size(g.grid) .* (p-g.a) ./ (g.b-g.a))
 end
+
+mutable struct InRadius{NN, N, T}
+    inner::NN
+    points::Vector{SVector{N,T}}
+    r::T
+    function InRadius(::Type{GridTree}, data, r, a, b)
+        length = r / √2
+        n = ceil.((b .- a) ./ length)
+        grid = zeros(Int, n...)
+        obj = InRadius(GridTree(a, b, grid), data, r)
+        for p in data
+            push!(obj, p)
+        end
+        return obj
+    end
+    function InRadius(::Type{T}, data, r, a, b) where {T<:NNTree}
+        return InRadius(T(data, Euclidean()), data, r)
+    end
+end
+
+function Base.in(r::InRadius, p)
+    return radius(r.inner, p) < r
+end
+Base.length(r::InRadius) = length(r.points)
+Base.getindex(r::InRadius, i) = r.points[i]
 
 widen(i::Int) = (i - 2):(i + 2)
 function radius(g::InRadius{<:GridTree}, p::SVector) where {N,T}
@@ -35,30 +62,5 @@ function Base.push!(g::InRadius{NN}, p::SVector) where {NN<:NNTree}
     g.inner = NN(g.points, Euclidean())
     return
 end
-
-mutable struct InRadius{NN, N, T}
-    inner::NN
-    points::Vector{SVector{N,T}}
-    r::T
-    function InRadius(::Type{GridTree}, data, r, a, b)
-        length = r / √2
-        n = ceil.((b .- a) ./ length)
-        grid = zeros(Int, n...)
-        obj = InRadius(GridTree(a, b, grid), data, r)
-        for p in data
-            push!(obj, p)
-        end
-        return obj
-    end
-    function InRadius(::Type{T}, data, r, a, b) where {T<:NNTree}
-        return InRadius(T(data, Euclidean()), data, r)
-    end
-end
-
-function Base.in(r::InRadius, p)
-    return radius(r.inner, p) < r
-end
-Base.length(r::InRadius) = length(r.points)
-Base.getindex(r::InRadius, i) = r.points[i]
 
 end
