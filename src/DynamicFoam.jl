445823bed8086374f738d07fca5a26103f3d72ca
Benoît Legat <benoit.legat@gmail.com>
2021-10-26 21:32:54 -0700

    Cleanup

--- src/DynamicFoam.jl
+++ src/DynamicFoam.jl
@@ -18,13 +18,5 @@ include("plot.jl")
 
 import MiniQhull, QHull, CDDLib, VoronoiDelaunay
 const LIBRARIES = [MiniQhull.delaunay, QHull.Library(), CDDLib.Library(:float), VoronoiDelaunay.DelaunayTessellation2D]
-#include("plotting.jl")
-
-
-#function main(K, a, b, delaunay_algo, centering=Barycenter(), scatterfun=scatter!, args...)
-#    points = random_points(K, a, b, args...)
-#    foam = Foam(points, delaunay_algo, centering)
-#    return viz(foam, K, a, b, delaunay_algo, centering, scatterfun, args...)
-#end
 
 end
